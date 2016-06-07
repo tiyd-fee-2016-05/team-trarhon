913e4c01a4aca359deaad48a66bc7402cf516ac4
Travis Johnson <travisjohnson@T-Ravs-Mac.local>
2016-06-07 16:59:53 -0400

    sidebar works

--- assets/js/octocat.js
+++ assets/js/octocat.js
@@ -26,7 +26,7 @@ $(function () {
 
   function show(template, model) {
     var fn = _.template($('#' + template).html(), { variable: 'm' });
-    $('.user-info').html(fn(model));
+    $('#profile').html(fn(model));
   }
 
 });
