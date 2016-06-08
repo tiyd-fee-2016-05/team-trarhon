3efb39e72f35b95d9c010d8cc5e2619c840aa34b
Travis Johnson <travisjohnson@T-Ravs-Mac.local>
2016-06-08 16:33:40 -0400

    repo-list done

--- assets/js/octocat.js
+++ assets/js/octocat.js
@@ -29,3 +29,42 @@ $(function () {
   }
 
 });
+
+
+// $('.snackz').click(function (){
+//   var menu = $(this).siblings().toggleClass("activate"); //here i target the class "activate" which then toggles the "display" property and sets it to "block".
+//   $("h3").not(menu).removeClass("activate");  // 'not'removes elements from the set of matched elements.
+// });
+
+
+
+
+// $(function () {
+//
+//   $('.user-form').on('submit', function (e) {
+//     e.preventDefault();
+//     console.log('submit');
+//
+//     var ghLogin = $('input[name="gh-login"]').val();
+//     $.getJSON('https://api.github.com/users/' + ghLogin)
+//       .done(showUser)
+//       .fail(showError);
+//   });
+//
+//   function showUser(user) {
+//     show('gh-user-template', user);
+//   }
+//
+//   function showError(req, status, err) {
+//     err = err || {};
+//     err.message = err.message || status;
+//     console.log(err);
+//     show('gh-error-template', { message: err });
+//   }
+//
+//   function show(template, model) {
+//     var fn = _.template($('#' + template).html(), { variable: 'm' });
+//     $('#profile').html(fn(model));
+//   }
+//
+// });
