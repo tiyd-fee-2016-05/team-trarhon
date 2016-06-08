5fdb1510189ad01a787110a1801fb266e411fe13
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-06-08 18:32:43 -0400

    Add some skull tags

--- assets/js/octocat.js
+++ assets/js/octocat.js
@@ -1,5 +1,43 @@
-console.log('word');
 
+
+
+
+
+
+
+
+
+// $(function () {
+//
+//   $('.user-form').on('submit', function (e) {
+//     e.preventDefault();
+//     console.log('submit');
+//
+//     var ghLogin = $('input[name="gh-login"]').val();
+//     $.getJSON('https://api.github.com/users/' + ghLogin)
+//       .done(showUser)
+//       .fail(showError);
+//   });
+//
+//   function showUser(user) {
+//     show('gh-user-template', user);
+//   }
+//
+//   function showError(req, status, err) {
+//     err = err || {};
+//     err.message = err.message || status;
+//     console.log(err);
+//     show('gh-error-template', { message: err });
+//   }
+//
+//   function show(template, model) {
+//     var fn = _.template($('#' + template).html(), { variable: 'm' });
+//     $('#profile').html(fn(model));
+//   }
+//
+// });
+//
+//
 
 $(function () {
 
@@ -8,12 +46,12 @@ $(function () {
     console.log('submit');
 
     var ghLogin = $('input[name="gh-login"]').val();
-    $.getJSON('https://api.github.com/users/' + ghLogin)
-      .done(showUser)
+    $.getJSON('https://api.github.com/users/' + ghLogin + "/repos")
+      .done(showRepo)
       .fail(showError);
   });
 
-  function showUser(user) {
+  function showRepo(repo) {
     show('gh-user-template', user);
   }
 
@@ -26,7 +64,7 @@ $(function () {
 
   function show(template, model) {
     var fn = _.template($('#' + template).html(), { variable: 'm' });
-    $('#profile').html(fn(model));
+    $('#repo-list').html(fn(model));
   }
 
 });
