537692412b76a7ea6b3569d07e4a383406bec1c4
Travis Johnson <travisjohnson@T-Ravs-Mac.local>
2016-06-08 16:57:29 -0400

    repo list done

--- assets/js/octocat.js
+++ assets/js/octocat.js
@@ -39,32 +39,32 @@ $(function () {
 
 
 
-// $(function () {
-//
-//   $('.user-form').on('submit', function (e) {
-//     e.preventDefault();
-//     console.log('submit');
-//
-//     var ghLogin = $('input[name="gh-login"]').val();
-//     $.getJSON('https://api.github.com/users/' + ghLogin)
-//       .done(showUser)
-//       .fail(showError);
-//   });
-//
-//   function showUser(user) {
-//     show('gh-user-template', user);
-//   }
-//
-//   function showError(req, status, err) {
-//     err = err || {};
-//     err.message = err.message || status;
-//     console.log(err);
-//     show('gh-error-template', { message: err });
-//   }
-//
-//   function show(template, model) {
-//     var fn = _.template($('#' + template).html(), { variable: 'm' });
-//     $('#profile').html(fn(model));
-//   }
-//
-// });
+$(function () {
+
+  $('.user-form').on('submit', function (e) {
+    e.preventDefault();
+    console.log('submit');
+
+    var ghLogin = $('input[name="gh-login"]').val();
+    $.getJSON('https://api.github.com/users/' + ghLogin)
+      .done(showUser)
+      .fail(showError);
+  });
+
+  function showRepo(repo) {
+    show('gh-repo-template', repo);
+  }
+
+  function showError(req, status, err) {
+    err = err || {};
+    err.message = err.message || status;
+    console.log(err);
+    show('gh-error-template', { message: err });
+  }
+
+  function show(template, model) {
+    var fn = _.template($('#' + template).html(), { variable: 'm' });
+    $('#repo-list').html(fn(model));
+  }
+
+});
