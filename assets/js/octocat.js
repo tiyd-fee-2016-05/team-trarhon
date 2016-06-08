9ed99624db6d15932af4662f9f6f0c8419e85789
Travis Johnson <travisjohnson@T-Ravs-Mac.local>
2016-06-08 18:32:39 -0400

    jSON is not my friend

--- assets/js/octocat.js
+++ assets/js/octocat.js
@@ -1,34 +1,34 @@
 
-
-$(function () {
-
-  $('.user-form').on('submit', function (e) {
-    e.preventDefault();
-    console.log('submit');
-
-    var ghLogin = $('input[name="gh-login"]').val();
-    $.getJSON('https://api.github.com/users/' + ghLogin)
-      .done(showUser)
-      .fail(showError);
-  });
-
-  function showUser(user) {
-    show('gh-user-template', user);
-  }
-
-  function showError(req, status, err) {
-    err = err || {};
-    err.message = err.message || status;
-    console.log(err);
-    show('gh-error-template', { message: err });
-  }
-
-  function show(template, model) {
-    var fn = _.template($('#' + template).html(), { variable: 'm' });
-    $('#profile').html(fn(model));
-  }
-
-});
+//
+// $(function () {
+//
+//   $('.user-form').on('submit', function (e) {
+//     e.preventDefault();
+//     console.log('submit');
+//
+//     var ghLogin = $('input[name="gh-login"]').val();
+//     $.getJSON('https://api.github.com/users/' + ghLogin)
+//       .done(showUser)
+//       .fail(showError);
+//   });
+//
+//   function showUser(user) {
+//     show('gh-user-template', user);
+//   }
+//
+//   function showError(req, status, err) {
+//     err = err || {};
+//     err.message = err.message || status;
+//     console.log(err);
+//     show('gh-error-template', { message: err });
+//   }
+//
+//   function show(template, model) {
+//     var fn = _.template($('#' + template).html(), { variable: 'm' });
+//     $('#profile').html(fn(model));
+//   }
+//
+// });
 
 
 // $('.snackz').click(function (){
@@ -43,16 +43,17 @@ $(function () {
 
   $('.user-form').on('submit', function (e) {
     e.preventDefault();
-    console.log('submit');
+    console.log('submit-repo');
 
     var ghLogin = $('input[name="gh-login"]').val();
-    $.getJSON('https://api.github.com/users/' + ghLogin)
-      .done(showUser)
+    $.getJSON('https://api.github.com/users/' + ghLogin + "/repos")
+      .done(showRepo)
       .fail(showError);
   });
 
   function showRepo(repo) {
     show('gh-repo-template', repo);
+    console.log("progress");
   }
 
   function showError(req, status, err) {
